450cf9216cecfe3508fd865fceabcfaafcedc20e
Greg Rychlewski <greg.rychlewski@gmail.com>
2019-07-28 12:43:15 -0400

    add build directory

--- build.sh
+++ build.sh
@@ -1,7 +1,28 @@
+# Remember to update this variable when publishing a new version
 APP_VERSION=1.0.0
 
+# This will embed develpment information into our app
 GIT_HASH=`git rev-parse HEAD`
 BUILD_TIME_UTC=`date -u '+%Y-%m-%d_%I:%M:%S%p'`
 GO_VERSION=`go version`
 
-go build -ldflags "-X 'main.goBuildVersion=${GO_VERSION}' -X main.buildTime=${BUILD_TIME_UTC} -X main.gitHash=${GIT_HASH} -X main.version=${APP_VERSION}" image_compare.go
+# List all target operating systems and architectures in space separated lists
+TARGET_OS='windows darwin'
+TARGET_ARCH='386 amd64'
+
+# Build app for each operating system/architecture combination
+for OS in ${TARGET_OS}; do
+  for ARCH in ${TARGET_ARCH}; do
+    BINARY_FILE="image_compare-${OS}-${ARCH}"
+
+    if [[ ${OS} == 'windows' ]]; then
+      BINARY_FILE=${BINARY_FILE}.exe
+    fi;
+
+    GOOS=${OS} GOARCH=${ARCH} \
+    go build \
+      -o build/${BINARY_FILE}\
+      -ldflags "-X 'main.goBuildVersion=${GO_VERSION}' -X main.buildTime=${BUILD_TIME_UTC} -X main.gitHash=${GIT_HASH} -X main.version=${APP_VERSION}"
+  done
+done
+
